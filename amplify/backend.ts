d9b9d01d806f5e8060ebefe3e780e974e1b77101
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-05-18 15:08:47 +0900

    Add first function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
+import { myFirstFunction } from './function/resource.js';
 
 defineBackend({
   auth,
   data,
+  myFirstFunction
 });
