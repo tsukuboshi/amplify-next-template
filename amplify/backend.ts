289ce97067446fe02f5d4b1d190de34a74355b94
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-06-03 13:35:16 +0900

    Delete function and torege

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,12 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
 import { data } from './data/resource.js';
-import { myFirstFunction } from './function/resource.js';
-import { storage } from './storage/resource';
 
 defineBackend({
   auth,
   data,
-  myFirstFunction,
-  storage,
+  // myFirstFunction,
+  // storage,
 });
